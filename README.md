aa6edbcfa6102a81e6cbb2e52a2073ad222505ce
Sebastien BENOIT <sb@beeone.42.fr>
2015-08-05 16:06:10 +0200

    features added and README updated

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# pyslack
+pyslack
+================
 Slack RTM Bot in Python
 
-INSTALL:
+INSTALL
+----------
+
+#### Require python-slackclient: https://github.com/slackhq/python-slackclient
+
+    pip install slackclient
 
 if you experience "InsecurePlatformWarning" error, please upgrade your ssl client
 
 http://stackoverflow.com/questions/29099404/ssl-insecureplatform-error-when-using-requests-package
-pip install --upgrade ndg-httpsclient
+
+    pip install --upgrade ndg-httpsclient
--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-import time, json, os, requests, urllib
+#!/usr/bin/python
+
+import time, json, os, requests, urllib, subprocess
 from slackclient import SlackClient
 
 #urllib3.disable_warnings()
@@ -43,6 +45,11 @@ def msg_snap(sc, msg):
     os.system("./imagesnap")
     post_file("snapshot.jpg")
 
+def msg_free(sc, msg):
+    proc = subprocess.Popen(["./free.sh"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    post_msg(out)
+
 def msg_usage(sc, msg):
     r = requests.get("http://enroll.42.fr/usage.php")
     post_msg(r.content)
@@ -60,24 +67,57 @@ def msg_cam(sc, msg):
     if (len(msg) == 1):
         k = config['cams'].keys()
         k.sort()
-        post_msg("\n".join(k))
+        res = "Cameras:"
+        for i in k:
+            res = res + "\n"
+            res = res + "*" + i + "* : "
+            cams = config['cams'][i]
+            res = res + "_" + ",".join(cams) + "_"
+        post_msg(res)
     else:
         if (len(config['cams']) > 0) and (msg[1] in config['cams'].keys()):
             for c in config['cams'][msg[1]]:
                 msg_cam_do(c)
         else:
             msg_cam_do(msg[1])
-        
+
+def msg_balance(sc, msg):
+    global config
+    r = requests.get(config['bank_base'] + "balance.php?login=" + msg[1] + "&key=" + config['bank_key'])
+    post_msg(r.content)
+
+def msg_credit(sc, msg):
+    global config
+    r = requests.get(config['bank_base'] + "credit.php?login=" + msg[1] + "&amount=" + msg[2] + "&key=" + config['bank_key'])
+    post_msg(r.content)
+
+def msg_pay(sc, msg):
+    global config
+    r = requests.get(config['bank_base'] + "pay.php?login=" + msg[1] + "&amount=" + msg[2] + "&key=" + config['bank_key'])
+    post_msg(r.content)
+
+            
 msg_fct = {
-    'help':  msg_help,
-    'load':  msg_load,
-    'snap':  msg_snap,
-    'usage': msg_usage,
-    'cam':   msg_cam
+    'help':     msg_help,
+    'load':     msg_load,
+    'snap':     msg_snap,
+    'free':     msg_free,
+    'usage':    msg_usage,
+    'cam':      msg_cam,
+    'balance':  msg_balance,
+    'credit':   msg_credit,
+    'pay':      msg_pay
 }
 
 msg_help = {
-    'cam': "cam [cam_name]"
+    'cam': "*cam* [cam_name] _# without arg show cam list, with arg upload cam pic_",
+    'load': "*load* _# show load of bot host_",
+    'snap': "*snap* _# upload a snapshot from host cam_",
+    'usage': "*usage* _# return number of people in the building_",
+    'balance': "*balance* <login> _# show user foodtruck balance_",
+    'credit': "*credit* <login> <amount> _# add amount to user foodtruck balance_",
+    'pay': "*pay* <login> <amount> _# remove amount from user foodtruck balance_",
+    'help': "*help* _# show this help_"
 }
 
 # {u'text': u'salut', u'ts': u'1437998018.000013', u'user': u'U03BC35LH', u'team': u'T03BDS0E2', u'type': u'message', u'channel': u'D086XDSTF'}
@@ -98,11 +138,16 @@ print(config['gid'])
 if sc.rtm_connect():
     while True:
         rtm = sc.rtm_read()
-        if (len(rtm) > 0):
-            for r in rtm:
-                if "type" in r:
-                    if r["type"] in funcdict.keys():
-                        funcdict[r["type"]](sc, r)
+        try:
+            if (len(rtm) > 0):
+                for r in rtm:
+                    if "type" in r:
+                        if r["type"] in funcdict.keys():
+                            funcdict[r["type"]](sc, r)
+        except Exception:
+            print("parse error: ")
+            print(rtm)
+            pass
         time.sleep(1)
 else:
     print "Connection Failed, invalid token?"
